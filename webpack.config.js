e7a37c4ea203cb0f4cdf6764ffb43b5f5b07e18f
Timeless Residents <business@satotakuya.jp>
2025-02-25 12:05:32 +0900

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const webpack = require("webpack");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
